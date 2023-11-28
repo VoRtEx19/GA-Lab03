c49cd598a8f8cdc4862d2ed3223ee8f4e39304c4
VoRtEx19 <lasvetlichnyy@edu.hse.ru>
2023-11-28 11:24:20 +0300

    v0.1
    - doesn't work properly yet

--- app/src/main/java/com/example/lab03/MainActivity.java
+++ app/src/main/java/com/example/lab03/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.lab03;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.lab03.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public static String name;
+
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        com.example.lab03.databinding.ActivityMainBinding binding =
+                ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        editText = binding.etActivity1EnterName;
+        final Button button = binding.bActivity1Next;
+
+        editText.setText(name);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        name = editText.getText().toString();
+        Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/lab03/SecondActivity.java
@@ -0,0 +1,36 @@
+package com.example.lab03;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.lab03.databinding.ActivitySecondBinding;
+
+public class SecondActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        com.example.lab03.databinding.ActivitySecondBinding binding =
+                ActivitySecondBinding.inflate(getLayoutInflater());
+        setContentView(R.layout.activity_second);
+
+        TextView textView = binding.tvActivity2TextEntered;
+        Button button = binding.bActivity2Back;
+
+        textView.setText(MainActivity.name);
+        Log.i("Activity2", "Text set" + textView.getText().toString());
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(SecondActivity.this, MainActivity.class);
+        startActivity(intent);
+    }
+}
